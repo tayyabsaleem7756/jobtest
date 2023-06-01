b01ab16af644d0fef7349bb5e235ba73caad0583
Tayyab Saleem <tayyabsaleem7756@gmail.com>
2023-06-01 14:53:35 +0500

    s

--- frontend/cypress/e2e/smokeTests/investor/jobtest.cy.js
+++ frontend/cypress/e2e/smokeTests/investor/jobtest.cy.js
@@ -1,6 +1,6 @@
 /// <reference types= "cypress" />
 
-describe('Investor Smoke - Invest As an Individual', () => {
+describe('Investor Smoke - Invest As an Individual f', () => {
 
     it('C12078: Create Fund/ Eligibility Criteria/ Approval of Eligibility', () => {
 
